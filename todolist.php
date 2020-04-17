80aba91fe402015cfaa30efaa7f9dc31edb04ef2
[solenn] <solenn.massot@laplateforme.io>
2020-04-17 11:54:23 +0200

    //bdd update

---
+++ todolist.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
+        <script src="script.js"></script>
+        <title>To Do List</title> 
+    </head>
+    <body>
+        ?>
+    </body>
+</html>
+<?php
+$_SESSION['user'] = true;
+$_SESSION['id'] = 1;
+?>
