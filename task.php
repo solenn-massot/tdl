538d5b24c354c73eec16cbb22e79b85e24281288
[solenn] <solenn.massot@laplateforme.io>
2020-04-21 16:14:43 +0200

    ya pas de css mais ça marche héhé

---
+++ task.php
@@ -0,0 +1,49 @@
+<?php
+include 'class/bdd.php';
+
+session_start();
+
+if(!isset($bdd))
+{
+    $bdd = new bdd;
+}
+
+if(isset($_POST['function']) || isset($_GET['function']))
+{
+    $function = $_POST['function'];
+
+    switch($function)
+    {
+        case "getTD_done":
+            $id = $_POST['id_user'];
+            $bdd->getTD_Done($id);
+        break;
+
+        case "supp":
+            $id = $_POST['id_task'];
+            $bdd->suppTask($id);
+        break;
+
+        case "done":
+            $id = $_POST['id_task'];
+            $bdd->doneTask($id);
+        break;
+
+        case "getTD_finish":
+            $id = $_POST['id_user'];
+            $bdd->getTD_Finish($id);
+        break;
+
+        case "add":
+            $id = $_POST['id_user'];
+            $titre = $_POST['titre'];
+            $description = $_POST['description'];
+            $deadline = $_POST['deadline'];
+            $bdd->create_Task($id, $titre, $description, $deadline);
+        break;
+    }
+}
+
+
+
+?>
--- todolist.php
+++ todolist.php
@@ -3,13 +3,16 @@
     <head>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
         <script src="script.js"></script>
-        <title>To Do List</title> 
     </head>
     <body>
-        ?>
+    <section id="todo">
+        <h1>Votre liste de tâches à faire</h1>
+
+    </section>
+    <section id="done">
+        <h1>Votre liste de tâches faites</h1>
+
+    </section>
+    <button id="create">Créer une tâche</button>
     </body>
 </html>
-<?php
-$_SESSION['user'] = true;
-$_SESSION['id'] = 1;
-?>
